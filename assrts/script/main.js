ec7028cd8abae0212ff60852c4641272b15bed99
kunal kale <kunalkale7080@gmail.com>
2023-10-20 14:30:58 +0530

    first commit

---
+++ assrts/script/main.js
@@ -0,0 +1,16 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+// minicircle
+
+function circleMouseFollower() {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector(
+      "#minicircle"
+    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+  });
+}
+
+circleMouseFollower();
